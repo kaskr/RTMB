951163a7fc64f4481fb9d6c57d1475288a512ef9
kaskr <kaskr@dtu.dk>
2023-01-18 17:10:11 +0100

    fix previous

--- RTMB/R/zzz.R
+++ RTMB/R/zzz.R
@@ -9,4 +9,4 @@
 
 ## For R 2.15.1 and later this also works. Note that calling loadModule() triggers
 ## a load action, so this does not have to be placed in .onLoad() or evalqOnLoad().
-loadModule("adfun", TRUE)
+loadModule("mod_adfun", TRUE)
