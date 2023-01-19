b1525534b59a9144f5e54f1bf20f524b1f6af018
Kasper Kristensen <kaskr@dtu.dk>
2023-01-19 13:35:41 +0100

    fix example

--- tmb_examples/linreg.R
+++ tmb_examples/linreg.R
@@ -3,12 +3,12 @@ set.seed(123)
 data <- list(Y = rnorm(10) + 1:10, x=1:10)
 parameters <- list(a=0, b=0, logSigma=0)
 
-f <- function(parameters) {
+f <- function(parms) {
     Y <- data$Y
     x <- data$x
-    a <- parameters$a
-    b <- parameters$b
-    logSigma <- parameters$logSigma
+    a <- parms$a
+    b <- parms$b
+    logSigma <- parms$logSigma
     ## FIXME:
     ## ADREPORT(exp(2*logSigma));
     nll = -sum(dnorm(Y, a+b*x, exp(logSigma), TRUE))
@@ -21,4 +21,4 @@ opt <- do.call("optim", obj)
 opt
 opt$hessian ## <-- FD hessian from optim
 obj$he()    ## <-- Analytical hessian
-sdreport(obj)
+TMB::sdreport(obj)
