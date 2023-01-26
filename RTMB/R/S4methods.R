9aa8eea42c8ee961dfc6ee4ef94313703187d098
Kasper Kristensen <kaskr@dtu.dk>
2023-01-26 19:25:21 +0100

    Add class 'adsparse'

--- RTMB/R/S4methods.R
+++ RTMB/R/S4methods.R
@@ -7,3 +7,14 @@ setMethod("Ops",
 setMethod("Ops",
           signature("advector", "sparseMatrix"),
           function(e1, e2) callGeneric( e1, magic(e2, TRUE) ) )
+
+setClass("adsparse",
+         slots=c(x="advector", i="integer", p="integer", Dim="integer"))
+
+setAs("sparseMatrix", "adsparse",
+      function(from) {
+          x <- from
+          x <- as(x, "generalMatrix")
+          x <- as(x, "CsparseMatrix")
+          new("adsparse", x=advector(x@x), i=x@i, p=x@p, Dim=x@Dim)
+      })
