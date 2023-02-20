8c81efd6a15cd35ef55dff91ab9c907c31bf700c
Kasper Kristensen <kaskr@dtu.dk>
2023-02-20 15:41:59 +0100

    simref: Improved inversion

--- RTMB/R/sim.R
+++ RTMB/R/sim.R
@@ -10,8 +10,20 @@
 simref <- function(n) {
     parent <- NULL
     value <- rep(NA, n)
-    update <- function(val, j) {
+    num.missing <- length(value)
+    complete <- function() (num.missing == 0)
+    ## 'finalize' is called when last element of simulation has been written.
+    ## A custom 'finalize' could be added to do:
+    ## - store the simulation in a safe place and
+    ## - override this 'simref' object with the simulation
+    finalize <- function(value) NULL
+    forward.update <- function(j) {
+        value[j]
+    }
+    reverse.update <- function(val, j) {
         NACHECK(value, j) <<- val
+        num.missing <<- num.missing - length(j)
+        if (complete()) finalize(value)
     }
     asS4(structure(environment(), class="simref"))
 }
@@ -42,11 +54,15 @@ is.na.simref <- function(x)is.na(x$value)
     x <- parent$value
     value <- NextMethod() ## Handles '...'
     ## ----
-    update <- function(val, j) {
+    forward.update <- function(j) {
+        value[j] <<- parent$forward.update( i[j] )
+        ## Implicit return: value[j]
+    }
+    reverse.update <- function(val, j) {
         ## Update 'value' of this object
         NACHECK(value, j) <<- val
         ## Pass updated 'value' to parent
-        parent$update( val, i[j] )
+        parent$reverse.update( val, i[j] )
     }
     if (all(!is.na(value)))
         return (value)
@@ -67,15 +83,22 @@ setMethod("show", "simref", function(object) {
     index <- seq_along(x)
     dim(index) <- dim(x)
     j <- as.integer(eval(cl))
-    x$update(value, j)
+    x$reverse.update(value, j)
     x
 }
 ##' @describeIn Simulation Equivalent of \link[base]{Ops}
 Ops.simref <- function(e1, e2) {
-    if (missing(e2)) stop("Not yet implemented")
+    if (missing(e2)) {
+        if (.Generic == "+")
+            return(e1)
+        else if (.Generic == "-")
+            return(0-e1)
+        else
+            stop("Cannot use '", .Generic, "' with single argument")
+    }
+    ## 'unknown' here means 'not completely known' i.e. there might be some elements known
     e1.unknown <- inherits(e1, "simref")
     e2.unknown <- inherits(e2, "simref")
-    if (e1.unknown && e2.unknown) stop("Not yet implemented")
     getval <- function(e) if (inherits(e, "simref")) e$value else e
     value <- callGeneric(getval(e1), getval(e2))
     n <- length(value)
@@ -88,38 +111,78 @@ Ops.simref <- function(e1, e2) {
         else
             rep(e, length.out=n)[j]
     }
-    ## update(): Solve wrt unknown
+    ## reverse.update(): Solve wrt unknown
     ##   ( e1 OP e2 ) [j] = val
-    if (e1.unknown) {
-        .Inverse <- c("+"="-", "-"="+", "*"="/", "/"="*")[.Generic]
-    } else {
-        ## e2.unknown
-        .Inverse <- c("+"="-", "-"="-", "*"="/", "/"="/")[.Generic]
-    }
+    ## e1.unknown
+    .Inverse1 <- c("+"="-", "-"="+", "*"="/", "/"="*")[.Generic]
+    ## e2.unknown
+    .Inverse2 <- c("+"="-", "-"="-", "*"="/", "/"="/")[.Generic]
+    ## Known case?
+    if (is.na(.Inverse1) || is.na(.Inverse2))
+        stop("Class 'simref' does not know generic '", .Generic, "'")
     if (.Generic %in% c("-", "/")) {
         ## Swap args
-        inverse <- function(e1, e2) .Primitive(.Inverse)(e2,e1)
+        inverse1 <- function(e1, e2) .Primitive(.Inverse1)(e2, e1)
+        inverse2 <- function(e1, e2) .Primitive(.Inverse2)(e2, e1)
     } else {
-        inverse <- match.fun(.Inverse)
+        inverse1 <- match.fun(.Inverse1)
+        inverse2 <- match.fun(.Inverse2)
     }
-    update <- function(val, j) {
-        ve1 <- s(e1, j) ## Now length(e1) = length(j) = length(val)
-        ve2 <- s(e2, j) ## Now length(e2) = length(j) = length(val)
-        NACHECK(value, j) <<- val
-        if (e1.unknown) e1$update(inverse(val, ve2), j)
-        if (e2.unknown) e2$update(inverse(val, ve1), j)
+    forward.update <- function(j) {
+        if (e1.unknown) {
+            if (length(e1) == n)
+                e1$forward.update(j)
+            else
+                e1$forward.update()
+        }
+        if (e2.unknown) {
+            if (length(e2) == n)
+                e2$forward.update(j)
+            else
+                e2$forward.update()
+        }
+        value[j] <<- match.fun(.Generic)(s(e1, j), s(e2, j))
+    }
+    if (e1.unknown && e2.unknown) {
+        reverse.update <- function(val, j) {
+            if (length(e1) != length(e2)) stop("")
+            for (i in seq_along(j)) {
+                e1$forward.update(j[i])
+                e2$forward.update(j[i])
+                NACHECK(value, j[i]) <<- val[i]
+                if (is.na(e1$value[j[i]] && is.na(e2$value[j[i]])))
+                    stop("Implicit simulation failed; Not enough information")
+                if (is.na(e1$value[j[i]]))
+                    e1$reverse.update(inverse1(val[i], e2$value[j[i]]), j[i])
+                if (is.na(e2$value[j[i]]))
+                    e2$reverse.update(inverse2(val[i], e1$value[j[i]]), j[i])
+            }
+        }
+    } else {
+        reverse.update <- function(val, j) {
+            ve1 <- s(e1, j) ## Now length(e1) = length(j) = length(val)
+            ve2 <- s(e2, j) ## Now length(e2) = length(j) = length(val)
+            NACHECK(value, j) <<- val
+            if (e1.unknown) e1$reverse.update(inverse1(val, ve2), j)
+            if (e2.unknown) e2$reverse.update(inverse2(val, ve1), j)
+        }
     }
     asS4(structure(environment(), class="simref"))
 }
 ##' @describeIn Simulation Equivalent of \link[base]{Math}
 Math.simref <- function(x) {
     .Inverse <- c("exp"="log", "log"="exp")[.Generic]
+    if (is.na(.Inverse))
+        stop("Class 'simref' does not know generic '", .Generic, "'")
     inverse <- match.fun(.Inverse)
     parent <- x
     value <- callGeneric(x$value)
-    update <- function(val, j) {
+    forward.update <- function(j) {
+        value[j] <- match.fun(.Generic)(parent$forward.update(j))
+    }
+    reverse.update <- function(val, j) {
         NACHECK(value, j) <<- val
-        parent$update(inverse(val), j)
+        parent$reverse.update(inverse(val), j)
     }
     asS4(structure(environment(), class="simref"))
 }
