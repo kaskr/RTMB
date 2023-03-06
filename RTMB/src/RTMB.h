f76a5b373773ddfe6e362217978646d37a071b04
Kasper Kristensen <kaskr@dtu.dk>
2023-03-06 15:21:32 +0100

    header cleanup

--- RTMB/src/RTMB.h
+++ RTMB/src/RTMB.h
@@ -1,10 +1,40 @@
 #include <Rcpp.h>
 #include "TMB.h"
 
-
+// AD type shorthands
 typedef TMBad::ad_aug ad;
 typedef Eigen::Map<Eigen::Matrix<ad, Eigen::Dynamic, Eigen::Dynamic> > MapMatrix;
 typedef Eigen::Map<const Eigen::Matrix<ad, Eigen::Dynamic, Eigen::Dynamic> > ConstMapMatrix;
+
+// AD scalar conversions ( Rcomplex <-> ad )
+Rcomplex ad2cplx(const ad &x);
+ad       cplx2ad(const Rcomplex &x);
+ad       ScalarInput(SEXP x_);
+// AD vector view of R representation
+ad* adptr(const Rcpp::ComplexVector &x);
+Rcpp::ComplexVector& as_advector(Rcpp::ComplexVector &x);
+// Dense matrix input/output
+ConstMapMatrix      MatrixInput  (const Rcpp::ComplexMatrix &x);
+Rcpp::ComplexMatrix MatrixOutput (const matrix<ad> &X);
+// Sparse matrix input/output
+Eigen::SparseMatrix<ad> SparseInput  (Rcpp::S4 x);
+Rcpp::S4                SparseOutput (const Eigen::SparseMatrix<ad> &S);
+
+// Tests
+bool is_advector (SEXP x);
+bool is_admatrix (SEXP x);
+bool is_adsparse (SEXP x);
+bool is_adscalar (SEXP x);
+bool valid(const ad &x);
+bool valid(Rcpp::ComplexVector x);
+bool ad_context();
+
+// AD input validity check
+#define CHECK_INPUT(x)                                                  \
+if (!is_advector(x))                                                    \
+  Rcpp::stop("'" #x "' must be 'advector' (lost class attribute?)" );   \
+ if (!valid(Rcpp::ComplexVector(x)))                                    \
+  Rcpp::stop("'" #x "' is not a valid 'advector' (constructed using illegal operation?)" );
 
 // Global Tape Configuration
 struct tape_config_t {
@@ -24,27 +54,3 @@ struct tape_config_t {
   bool mvnorm_atomic   ();
 };
 extern tape_config_t tape_config;
-
-Rcomplex ad2cplx(const ad &x);
-ad cplx2ad(const Rcomplex &x);
-ad* adptr(const Rcpp::ComplexVector &x);
-bool is_advector (SEXP x);
-bool is_admatrix (SEXP x);
-bool is_adsparse (SEXP x);
-bool is_adscalar (SEXP x);
-bool valid(const ad &x);
-bool valid(Rcpp::ComplexVector x);
-bool ad_context();
-Rcpp::ComplexVector& as_advector(Rcpp::ComplexVector &x);
-
-#define CHECK_INPUT(x)                                                  \
-if (!is_advector(x))                                                    \
-  Rcpp::stop("'" #x "' must be 'advector' (lost class attribute?)" );   \
- if (!valid(Rcpp::ComplexVector(x)))                                    \
-  Rcpp::stop("'" #x "' is not a valid 'advector' (constructed using illegal operation?)" );
-
-ConstMapMatrix MatrixInput(const Rcpp::ComplexMatrix &x);
-Rcpp::ComplexMatrix MatrixOutput(const matrix<ad> &X);
-Eigen::SparseMatrix<ad> SparseInput(Rcpp::S4 x);
-Rcpp::S4 SparseOutput (const Eigen::SparseMatrix<ad> &S);
-ad ScalarInput(SEXP x_);
