71e4b638b66c69b6eaad93b9c412b7f0de4c9adb
Kasper Kristensen <kaskr@dtu.dk>
2023-03-28 15:13:56 +0200

    message

--- RTMB/src/config.h
+++ RTMB/src/config.h
@@ -2,6 +2,6 @@
 
 #include <Rcpp.h>
 // Any failed internal assertion sends Rcpp::exception
-#define TMB_ABORT Rcpp::stop("Bailing out")
+#define TMB_ABORT Rcpp::stop("TMB unexpected")
 // Catch *all* std exceptions (not just bad_alloc)
 #define TMB_CATCH catch(std::exception& excpt)
