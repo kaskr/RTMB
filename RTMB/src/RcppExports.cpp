c3e995bf28996136bec0a661fdbd0d55234eecf8
Kasper Kristensen <kaskr@dtu.dk>
2023-05-04 17:58:51 +0200

    sparse matrix exponential (C code)

--- RTMB/src/RcppExports.cpp
+++ RTMB/src/RcppExports.cpp
@@ -703,6 +703,20 @@ BEGIN_RCPP
     return rcpp_result_gen;
 END_RCPP
 }
+// expATv
+Rcpp::ComplexMatrix expATv(SEXP AT, Rcpp::ComplexMatrix v, Rcpp::ComplexVector N, Rcpp::List cfg);
+RcppExport SEXP _RTMB_expATv(SEXP ATSEXP, SEXP vSEXP, SEXP NSEXP, SEXP cfgSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< SEXP >::type AT(ATSEXP);
+    Rcpp::traits::input_parameter< Rcpp::ComplexMatrix >::type v(vSEXP);
+    Rcpp::traits::input_parameter< Rcpp::ComplexVector >::type N(NSEXP);
+    Rcpp::traits::input_parameter< Rcpp::List >::type cfg(cfgSEXP);
+    rcpp_result_gen = Rcpp::wrap(expATv(AT, v, N, cfg));
+    return rcpp_result_gen;
+END_RCPP
+}
 // TapedEval
 Rcpp::ComplexVector TapedEval(Rcpp::Function F, Rcpp::ComplexVector i);
 RcppExport SEXP _RTMB_TapedEval(SEXP FSEXP, SEXP iSEXP) {
@@ -775,6 +789,7 @@ static const R_CallMethodDef CallEntries[] = {
     {"_RTMB_dgmrf0", (DL_FUNC) &_RTMB_dgmrf0, 3},
     {"_RTMB_SparseArith2", (DL_FUNC) &_RTMB_SparseArith2, 3},
     {"_RTMB_math_expm", (DL_FUNC) &_RTMB_math_expm, 1},
+    {"_RTMB_expATv", (DL_FUNC) &_RTMB_expATv, 4},
     {"_RTMB_TapedEval", (DL_FUNC) &_RTMB_TapedEval, 2},
     {"_rcpp_module_boot_mod_adfun", (DL_FUNC) &_rcpp_module_boot_mod_adfun, 0},
     {NULL, NULL, 0}
