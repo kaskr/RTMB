192040215e138968f8208d13e3004b4576b582c0
Kasper Kristensen <kaskr@dtu.dk>
2023-06-03 10:44:48 +0200

    fft init

--- RTMB/src/math_complex.cpp
+++ RTMB/src/math_complex.cpp
@@ -11,6 +11,7 @@
 */
 
 #include "RTMB.h"
+#include <unsupported/Eigen/FFT>
 
 // Some extra interface helper functions
 typedef std::complex<ad> adcplx;
@@ -82,5 +83,66 @@ Rcpp::ComplexVector Math1_complex(const Rcpp::ComplexVector &x, std::string op) 
   else Rf_error("'%s' not implemented", op.c_str());
 #undef CALL
 #undef CUMC
+  return as_advector(y);
+}
+
+namespace TMBad {
+template<bool adjoint=false>
+struct FFTOp : global::DynamicOperator< -1 , -1 > {
+  typedef std::complex<double> cplx;
+  static const bool have_input_size_output_size = true;
+  static const bool add_forward_replay_copy = true;
+  size_t n;
+  Index input_size()  const { return n; }
+  Index output_size() const { return n; }
+  FFTOp (size_t n) : n(n) { }
+  void forward(ForwardArgs<double> &args) {
+    Eigen::FFT<double> fft;
+    std::vector<double> buf(n);
+    for (size_t i=0; i<n; i++) buf[i] = args.x(i);
+    cplx* src = (cplx*) buf.data();
+    cplx* dest = (cplx*) args.y_ptr(0);
+    if (!adjoint)
+      fft.fwd(dest, src, n/2);
+    else
+      fft.inv(dest, src, n/2);
+  }
+  void reverse(ReverseArgs<double> &args) {
+    Eigen::FFT<double> fft;
+    std::vector<double> buf(n);
+    cplx* dest = (cplx*) buf.data();
+    cplx* src = (cplx*) args.dy_ptr(0);
+    if (adjoint)
+      fft.fwd(dest, src, n/2);
+    else
+      fft.inv(dest, src, n/2);
+    for (size_t i=0; i<n; i++) args.dx(i) += buf[i];
+  }
+  template <class Type> void forward(ForwardArgs<Type> &args) {
+    TMBAD_ASSERT(false);
+  }
+  template <class Type> void reverse(ReverseArgs<Type> &args) {
+    //TMBad::ad_segment seg(args.py(0), n);
+    //TMBad::ad_segment res = TMBad::get_glob()->getOperator<FFTOp<!adjoint>>(n)(seg);
+    // Not yet implemented
+    ASSERT(false);
+  }
+  const char* op_name() {return adjoint ? "iFFT" : "FFT";}
+};
+}
+
+// [[Rcpp::export]]
+Rcpp::ComplexVector fft(const Rcpp::ComplexVector &x, bool adjoint=false) {
+  CHECK_INPUT(x);
+  size_t n = x.size();
+  ad* X = adptr(x);
+  // Add to tape
+  std::vector<ad> X_(X, X + n);
+  std::vector<ad> Y_ = TMBad::global::Complete<TMBad::FFTOp<> >(n) (X_);
+  // Pass to R
+  Rcpp::ComplexVector y(n);
+  for (size_t j=0; j < n; j++) {
+    y[j] = ad2cplx(Y_[j]);
+  }
   return as_advector(y);
 }
