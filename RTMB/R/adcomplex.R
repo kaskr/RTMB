e9249af1c906990af553c81bed0835553a216a4f
Kasper Kristensen <kaskr@dtu.dk>
2023-06-06 10:18:50 +0200

    fix

--- RTMB/R/adcomplex.R
+++ RTMB/R/adcomplex.R
@@ -2,10 +2,12 @@ setClass("adcomplex",
          slots=c(real="advector", imag="advector"))
 adcomplex <- function(real, imag=rep(advector(0), length(real))) {
     real <- advector(real)
+    dim(imag) <- dim(real)
     new("adcomplex", real=real, imag=imag)
 }
 Re.adcomplex <- function(x) x@real
 Im.adcomplex <- function(x) x@imag
+dim.adcomplex <- function(x) dim(Re(x))
 Conj.adcomplex <- function(x) adcomplex(Re(x), -Im(x))
 Mod.adcomplex <- function(x) sqrt(Re(x)*Re(x)+Im(x)*Im(x))
 "+.adcomplex" <- function(x, y) {
@@ -34,7 +36,7 @@ resplit <- function(x) {
     dim(x) <- c(2, length(x)/2)
     adcomplex(x[1,], x[2,])
 }
-setMethod("fft", "advector",
+setMethod("fft", "adcomplex",
           function(z, inverse) {
               ## if (!ad_context()) { ## Workaround
               ##     F <- .MakeTape(function(...)
@@ -42,9 +44,14 @@ setMethod("fft", "advector",
               ##     return advector(F$eval(numeric(0)))
               ## }
               d <- dim(z)
+              if (is.null(d)) d0 <- length(z) else d0 <- d
               z <- unsplit(z)
-              ans <- fft_complex(z, d)
+              ans <- fft_complex(z, d0)
               ans <- resplit(ans)
               dim(ans) <- d
               ans
+          })
+setMethod("fft", "advector",
+          function(z, inverse) {
+              fft(adcomplex(z), inverse)
           })
