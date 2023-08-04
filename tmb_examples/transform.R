37a73df332435364871e72ea6c8ecb88ae6f3103
Kasper Kristensen <kaskr@dtu.dk>
2023-08-04 11:19:54 +0200

    example tidy

--- tmb_examples/transform.R
+++ tmb_examples/transform.R
@@ -22,33 +22,19 @@ y <- x + rnorm(n, sd=sd)
 data <- list(y=y)
 parameters <- list(phi=0, shape=1, scale=1, sd=1, u=u*0)
 
-dar1 <- function(x, phi, log=FALSE) {
-    ans <-
-        dnorm(x[1], 0, 1, log=TRUE) +
-        sum(dnorm( x[-1] - phi * x[-length(x)], 0, sqrt(1-phi*phi), log=TRUE ))
-    if (log) ans else exp(ans)
-}
-
-func <- function(phi,
-                 shape,
-                 scale,
-                 sd,
-                 u) {
-    y <- data$y
+func <- function(parms) {
+    getAll(parms, data, warn=FALSE)
     y <- OBS(y) ## Optional
     res <- 0
-    res <- res - dar1(u, phi, log=TRUE)
+    res <- res - dautoreg(u, phi=phi, log=TRUE)
     unif <- pnorm(u)
     x <- qgamma(unif, shape, scale=scale)
     res <- res - sum(dnorm(y, x, sd, log=TRUE))
     res
 }
-do.call(func,parameters) ## 27638.34
+func(parameters) ## 27638.34
 
-obj <- MakeADFun(
-    function(p)do.call("func",p),parameters=parameters,
-    random="u"
-    )
+obj <- MakeADFun(func, parameters=parameters, random="u")
 obj$fn()
 opt <- nlminb(obj$par,obj$fn,obj$gr)
 sdr <- sdreport(obj)
