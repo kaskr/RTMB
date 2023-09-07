eb70968cce7420d48b1256ab810c16764d058978
Kasper Kristensen <kaskr@dtu.dk>
2023-09-07 17:06:57 +0200

    parallel build => set cores

--- RTMB/R/zzz.R
+++ RTMB/R/zzz.R
@@ -10,3 +10,10 @@
 ## For R 2.15.1 and later this also works. Note that calling loadModule() triggers
 ## a load action, so this does not have to be placed in .onLoad() or evalqOnLoad().
 loadModule("mod_adfun", TRUE)
+
+.onLoad <- function(libname, pkgname) {
+    parallel <- attr(.Call(getFramework), "openmp")
+    if (parallel) {
+        TMB::openmp(parallel::detectCores(), autopar=TRUE, DLL="RTMB")
+    }
+}
