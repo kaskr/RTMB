f5565580e4683bc5e155ff3c3fa257121fd7da86
Kasper Kristensen <kaskr@dtu.dk>
2023-11-27 16:29:50 +0100

    doc-update

--- RTMB/R/interpol.R
+++ RTMB/R/interpol.R
@@ -1,14 +1,39 @@
 ##' Interpolation
 ##'
 ##' Some interpolation methods are available to be used as part of 'RTMB' objective functions.
+##'
+##' `interpol1Dfun` and `interpol2Dfun` are kernel smoothers useful in the case where you need a 3rd order \emph{smooth} representation of a \emph{data} vector or matrix.
+##' A typical use case is when a high-resolution map needs to be accessed along a random effect trajectory.
+##' Both 1D and 2D cases accept an 'interpolation radius' parameter (default R=2) controlling the degree of smoothness. Note, that only the value R=1 will match the data exactly, while higher radius trades accuracy for smoothness. Note also that these smoothers do not attempt to extrapolate: The returned value will be `NaN` outside the valid range (`xlim` / `ylim`).
+##'
+##' `splinefun` mimics the corresponding `stats` function. The AD implementation (in contrast to `interpol1Dfun`) works for parameter dependent y-coordinates.
+##'
 ##' @rdname Interpolation
 ##' @name Interpolation
 ##' @param xlim Domain of x
 ##' @param ylim Domain of y
+##' @param z Matrix to be interpolated
 ##' @param ... Configuration parameters
 ##' @examples
+##' ## ======= interpol1D
+##' ## R=1 => exact match of observations
+##' f <- interpol1Dfun(sin(1:10), R=1)
+##' layout(t(1:2))
+##' plot(sin(1:10))
+##' plot(f, 1, 10, add=TRUE)
+##' title("R=1")
+##' F <- MakeTape(f, 0)
+##' F3 <- F$jacfun()$jacfun()$jacfun()
+##' plot(Vectorize(F3), 1, 10)
+##' title("3rd derivative")
+##' ## ======= interpol2D
 ##' f <- interpol2Dfun(volcano, xlim=c(0,1), ylim=c(0,1))
 ##' F <- MakeTape(function(x) f(x[1],x[2]), c(.5,.5))
+##' ## ======= splinefun
+##' T <- MakeTape(function(x){
+##'    S <- splinefun(1:10, sin(x))
+##'    S(4:6)
+##' }, 1:10)
 NULL
 
 ##' @describeIn Interpolation Construct a kernel smoothed representation of a vector.
@@ -38,6 +63,10 @@ interpol2Dfun <- function(z, xlim=c(1,nrow(z)), ylim=c(1,ncol(z)), ...) {
 }
 
 setGeneric("splinefun")
+##' @describeIn Interpolation Construct a spline function.
+##' @param x spline x coordinates
+##' @param y spline y coordinates
+##' @param method Same as for the stats version, however only the three first are available.
 setMethod("splinefun", signature(x="ANY",
                                  y="advector",
                                  method="ANY",
