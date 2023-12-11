97631829938b318f5e9a49e49024fe24efb69a68
Kasper Kristensen <kaskr@dtu.dk>
2023-12-11 14:09:55 +0100

    FIXME: Must tweak Rcpp output with extra load hook

--- rtmbTest2/src/RcppExports.cpp
+++ rtmbTest2/src/RcppExports.cpp
@@ -28,7 +28,9 @@ static const R_CallMethodDef CallEntries[] = {
     {NULL, NULL, 0}
 };
 
+void rtmb_set_shared_pointers();
 RcppExport void R_init_rtmbTest2(DllInfo *dll) {
     R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
     R_useDynamicSymbols(dll, FALSE);
+    rtmb_set_shared_pointers();
 }
