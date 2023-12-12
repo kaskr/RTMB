14e7ee0bad35ee6e858305eebd4d07430290a7ef
Kasper Kristensen <kaskr@dtu.dk>
2023-12-12 14:48:43 +0100

    oops

--- rtmbTest2/src/TMB.cpp
+++ rtmbTest2/src/TMB.cpp
@@ -8,7 +8,7 @@
 #include <R_ext/Visibility.h>
 void rtmb_set_shared_pointers() {
   typedef SEXP(*funptr)(SEXP);
-  funptr fun = (funptr) R_GetCCallable("RTMB", "getSetGlobalPtr");
+  funptr fun = (funptr) R_FindSymbol("getSetGlobalPtr", "RTMB", NULL);
   SEXP ptr = fun(R_NilValue);
   TMBad::global_ptr = (TMBad::global**) R_ExternalPtrAddr(ptr);
 }
