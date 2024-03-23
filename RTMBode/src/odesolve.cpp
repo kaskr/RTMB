54a1047e9a3a8b36b292a4f6dec03af31e011e8b
Kasper Kristensen <kaskr@dtu.dk>
2024-03-23 09:33:44 +0100

    use in ode c code

--- RTMBode/src/odesolve.cpp
+++ RTMBode/src/odesolve.cpp
@@ -1,17 +1,18 @@
 #include <RcppEigen.h>
 
-typedef void * (*FUN_PTR)(SEXP f, const Eigen::VectorXd &x, Eigen::VectorXd &y);
+typedef void * (*FUN_PTR)(void*);
 
-static SEXP PTR;
-static Eigen::VectorXd X;
-static Eigen::VectorXd Y;
+static void* F;
+static double* X;
+static double* Y;
+static FUN_PTR run_forward;
 
 extern "C"
-SEXP set_pointers(SEXP ptr, SEXP x, SEXP y) {
-  if (!R_ExternalPtrAddr(ptr)) Rf_error("oops!");
-  PTR = ptr;
-  X = Eigen::Map<Eigen::VectorXd> (REAL(x), LENGTH(x));
-  Y = Eigen::Map<Eigen::VectorXd> (REAL(y), LENGTH(y));
+SEXP set_pointers(SEXP tape, SEXP x, SEXP y) {
+  F = R_ExternalPtrAddr(tape);
+  X = (double*) R_ExternalPtrAddr(x);
+  Y = (double*) R_ExternalPtrAddr(y);
+  run_forward = (FUN_PTR) R_GetCCallable("RTMB", "ptr_forward");
   return R_NilValue;
 }
 
@@ -19,10 +20,9 @@ SEXP set_pointers(SEXP ptr, SEXP x, SEXP y) {
 extern "C"
 void desolve_derivs (int *neq, double *t, double *y, double *ydot,
                      double *yout, int *ip) {
-  static FUN_PTR tmb_forward = (FUN_PTR) R_GetCCallable("RTMB", "tmb_forward");
   X[0] = *t;
   for (int i=0; i < *neq; i++) X[i+1] = y[i];
-  tmb_forward(PTR, X, Y);
+  run_forward(F);
   for (int i=0; i < *neq; i++) ydot[i] = Y[i];
 }
 
