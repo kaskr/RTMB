4967a3c77b7d30b835a068d15e47e3d85df11382
Kasper Kristensen <kaskr@dtu.dk>
2024-08-12 15:52:45 +0200

    chol interface

---
+++ RTMB/R/chol.R
@@ -0,0 +1,28 @@
+lchol <- ADjoint(
+    function(x) {
+        t(chol(t(x)))
+    },
+    function(x, y, dy) {
+        if (!inherits(x, "advector")) {
+            ## Fast lapack based kernel
+            S <- sytrisol(y, dy)
+            diag(S) <- .5 * diag(S)
+            S
+        } else {
+            ## Slow rewritten version
+            T <- t(y) %*% dy
+            u <- upper.tri(T, FALSE)
+            T[u] <- t(T)[u]
+            Linv <- solve(y)
+            S <- t(Linv) %*% T %*% Linv
+            S[u] <- 0
+            diag(S) <- .5 * diag(S)
+            S
+        }
+    },
+    name = "lchol")
+
+##' @describeIn ADmatrix AD matrix cholesky
+chol.advector <- function(x) {
+    t(lchol(t(x)))
+}
