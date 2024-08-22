0043f4a13a9be9b902a6536a95cf0cbeff099b38
Kasper Kristensen <kaskr@dtu.dk>
2024-08-22 15:45:16 +0200

    fix tests

--- RTMB/inst/tinytest/test-advector.R
+++ RTMB/inst/tinytest/test-advector.R
@@ -9,12 +9,15 @@ expect_true(is(outer(x,x,"+"),   "advector"))
 expect_true(is(outer(1:3,x,"+"), "advector"))
 expect_true(is(outer(x,1:3,"+"), "advector"))
 
-## Method call: outer -> tcrossprod
-expect_true(is(outer(x,x),   "advector"))
-expect_true(is(outer(1:3,x), "advector"))
-expect_true(is(outer(x,1:3), "advector"))
+## See ?crossprod: **These are generic functions since R 4.4.0**
+if (getRversion() >= "4.4.0") {
+    ## Method call: outer -> tcrossprod
+    expect_true(is(outer(x,x),   "advector"))
+    expect_true(is(outer(1:3,x), "advector"))
+    expect_true(is(outer(x,1:3), "advector"))
 
-## Method call: kronecker -> outer -> tcrossprod
-expect_true(is(kronecker(x,x),   "advector"))
-expect_true(is(kronecker(1:3,x), "advector"))
-expect_true(is(kronecker(x,1:3), "advector"))
+    ## Method call: kronecker -> outer -> tcrossprod
+    expect_true(is(kronecker(x,x),   "advector"))
+    expect_true(is(kronecker(1:3,x), "advector"))
+    expect_true(is(kronecker(x,1:3), "advector"))
+}
