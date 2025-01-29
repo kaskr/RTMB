088f79478f379ae98a205b32f0d35fd9bd7a32c3
Kasper Kristensen <kaskr@dtu.dk>
2025-01-29 16:58:06 +0100

    simplify

--- RTMB/src/subset.cpp
+++ RTMB/src/subset.cpp
@@ -4,17 +4,13 @@
 // [[Rcpp::export]]
 ADrep subset(ADrep x,
              ADrep i) {
-  Eigen::Map<Eigen::Array<ad, -1, 1> > X(adptr(x), x.size());
-  Eigen::Map<Eigen::Array<ad, -1, 1> > Y(adptr(i), i.size());
-  vector<ad> ans = atomic::subset(vector<ad>(X), vector<ad>(Y));
+  vector<ad> ans = atomic::subset<ad>(x, i);
   return ADrep(ans.data(), ans.data() + ans.size());
 }
 
 // [[Rcpp::export]]
 ADrep findInterval(ADrep x,
                    ADrep i) {
-  Eigen::Map<Eigen::Array<ad, -1, 1> > X(adptr(x), x.size());
-  Eigen::Map<Eigen::Array<ad, -1, 1> > Y(adptr(i), i.size());
-  vector<ad> ans = atomic::findInterval(vector<ad>(X), vector<ad>(Y));
+  vector<ad> ans = atomic::findInterval<ad>(x, i);
   return ADrep(ans.data(), ans.data() + ans.size());
 }
