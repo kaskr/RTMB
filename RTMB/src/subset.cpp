de36f86007e382c22cca9faa2ad06621b8a2c52c
Kasper Kristensen <kaskr@dtu.dk>
2025-02-02 12:48:54 +0100

    rename

--- RTMB/src/subset.cpp
+++ RTMB/src/subset.cpp
@@ -2,23 +2,23 @@
 #include "RTMB.h"
 
 // [[Rcpp::export]]
-ADrep subset(ADrep x,
-             ADrep i) {
+ADrep subset_ad(ADrep x,
+                ADrep i) {
   return atomic::subset<ad>(x, i);
 }
 
 // [[Rcpp::export]]
-ADrep findInterval(ADrep x,
-                   ADrep i) {
+ADrep findInterval_ad(ADrep x,
+                      ADrep i) {
   return atomic::findInterval<ad>(x, i);
 }
 
 // [[Rcpp::export]]
-ADrep order(ADrep x) {
+ADrep order_ad(ADrep x) {
   return atomic::order<ad>(x);
 }
 
 // [[Rcpp::export]]
-ADrep sort(ADrep x) {
+ADrep sort_ad(ADrep x) {
   return atomic::sort<ad>(x);
 }
