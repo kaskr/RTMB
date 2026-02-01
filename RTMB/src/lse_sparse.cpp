64881c5de9289688ca73ee48be6930b2a00cb9dd
Kasper Kristensen <kaskr@dtu.dk>
2026-02-01 09:49:29 +0100

    Sparse logspace_sum

---
+++ RTMB/src/lse_sparse.cpp
@@ -0,0 +1,54 @@
+// [[Rcpp::depends(TMB)]]
+#include "RTMB.h"
+
+template<bool sparse=false>
+struct LogSpaceSumOp : TMBad::global::DynamicOperator< -1 , 1 > {
+  size_t n;
+  static const bool have_input_size_output_size = true; // FIXME: Should give compile time error if 'false'
+  static const bool add_forward_replay_copy = true;
+  TMBad::Index input_size()  const { return this->n; }
+  TMBad::Index output_size() const { return 1; }
+  LogSpaceSumOp (size_t n) : n(n) { }
+  void forward(TMBad::ForwardArgs<TMBad::Scalar> &args) {
+    TMBad::Scalar Max = -INFINITY;
+    for (size_t i=0; i<n; i++) {
+      if (Max < args.x(i)) Max = args.x(i) ;
+    }
+    args.y(0) = 0;
+    for (size_t i=0; i<n; i++) {
+      args.y(0) += exp( args.x(i) - Max );
+    }
+    args.y(0) = Max + log(args.y(0));
+  }
+  template<class Type>
+  void forward(TMBad::ForwardArgs<Type> &args) {
+    Rcpp::stop("unsupported method");
+  }
+  template <class Type>
+  Type SPDERIV(Type x) { return ( sparse ? Type(TMBad::sparseDeriv(x)) : x ); }
+  template <class Type> void reverse(TMBad::ReverseArgs<Type> &args) {
+    for (size_t i=0; i<n; i++) {
+      args.dx(i) += SPDERIV ( exp( args.x(i) - args.y(0) ) * args.dy(0) );
+    }
+  }
+  void reverse(TMBad::ReverseArgs<TMBad::Writer> &args) {
+    Rcpp::stop("LSE source writer not available");
+  }
+  const char* op_name() {return sparse ? "LSE0" : "LSE"; }
+};
+                          
+// [[Rcpp::export]]
+ADrep LSE0(ADrep x) {
+  std::vector<ad> x_(x.adptr(), x.adptr() + x.size());
+  TMBad::global::Complete<LogSpaceSumOp<true> > op(x.size());
+  std::vector<ad> y_ = op(x_);
+  return ADrep(y_.data(), y_.data() + y_.size());
+}
+
+// [[Rcpp::export]]
+ADrep LSE(ADrep x) {
+  std::vector<ad> x_(x.adptr(), x.adptr() + x.size());
+  TMBad::global::Complete<LogSpaceSumOp<false> > op(x.size());
+  std::vector<ad> y_ = op(x_);
+  return ADrep(y_.data(), y_.data() + y_.size());
+}
