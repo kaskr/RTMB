174dd59da6145733976091595925adef7b84821c
Kasper Kristensen <kaskr@dtu.dk>
2026-05-20 13:27:21 +0200

    fix previous

--- RTMB/R/experimental.R
+++ RTMB/R/experimental.R
@@ -177,17 +177,19 @@ gpu_atomic <- function(F, verbose=TRUE) {
                src)
   system(cmd)
   dyn.load(dll)
-  nrep_prev <- 0
+  nrep_prev <- 0L
   isInt <- function(x) round(x) == x
   f <- function(x, blksize=32L) {
     nrep <- length(x) / length(inv)
     if (!isInt(nrep)) stop("Invalid length")
+    nrep <- as.integer(nrep)
     if (nrep != nrep_prev) {
       value <- rep(F$data.frame()$Value, each=nrep)
       .C("dev_alloc", length(value), PACKAGE=DLL)
       .C("set_value", value, 0L, length(value), PACKAGE=DLL)
       nrep_prev <<- nrep
     }
+    .C("set_value", x, inv[1] * nrep, length(inv) * nrep, PACKAGE=DLL)
     if ( !isInt (nrep / blksize) ) blksize <- 1
     parms <- as.integer(c(blksize, nrep/blksize))
     .C("forward_kernel", parms, PACKAGE=DLL)
