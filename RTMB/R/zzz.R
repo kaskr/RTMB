40f9e0b0a9dd6c3e1301a4b13a27141d18d356aa
Kasper Kristensen <kaskr@dtu.dk>
2026-09-17 15:10:53 +0200

    Store TMB build version

--- RTMB/R/zzz.R
+++ RTMB/R/zzz.R
@@ -10,3 +10,5 @@
 ## For R 2.15.1 and later this also works. Note that calling loadModule() triggers
 ## a load action, so this does not have to be placed in .onLoad() or evalqOnLoad().
 loadModule("mod_adfun", TRUE)
+
+.TMB.build.version <- TMB::TMB.Version()
